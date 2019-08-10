8ad21afe114d3db785a5ae99cb695452861f6989
casper <133casper@gmail.com>
2019-08-10 21:09:53 +0300

    update and getById methods in UserRepository

--- src/main/java/com/casper/userslibrary/model/repository/wrapper/UserFileRepository.java
+++ src/main/java/com/casper/userslibrary/model/repository/wrapper/UserFileRepository.java
@@ -7,35 +7,14 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserFileRepository implements UserRepository {
-    @Override
-    public void save(User user) {
-        File usersDir = new File("users");
-
-        if (usersDir.exists()) {
-            FileFilter fileFilter = file -> file.getName().endsWith(".user");
-            File[] userFiles = usersDir.listFiles(fileFilter);
-
-            if (userFiles != null) {
-                long lastUserId = getLastUserId(userFiles);
-
-                if (lastUserId != -1) {
-                    String newUserFileName = (lastUserId + 1) + ".user";
-                    File userFile = new File(usersDir, newUserFileName);
-                    saveUserToFile(user, userFile);
-                }
-            }
-        } else if (usersDir.mkdir()) {
-            File userFile = new File(usersDir, "1.user");
-            saveUserToFile(user, userFile);
-        }
-    }
-
     private long getLastUserId(File[] userFiles) {
         long[] idArray = new long[userFiles.length];
 
@@ -80,9 +59,64 @@ public class UserFileRepository implements UserRepository {
         }
     }
 
+    private User parseUserFileText(String text) {
+        Pattern splitPattern = Pattern.compile("\n\n");
+        String[] userInfoParts = splitPattern.split(text);
+
+        if (userInfoParts.length == 5) {
+            String firstName = userInfoParts[0];
+            String lastName = userInfoParts[1];
+            String email = userInfoParts[2];
+
+            splitPattern = Pattern.compile("\n");
+            String[] roles = splitPattern.split(userInfoParts[3]);
+            String[] phoneNumbers = splitPattern.split(userInfoParts[4]);
+
+            return User
+                    .builder()
+                        .firstName(firstName)
+                        .lastName(lastName)
+                        .email(email)
+                        .roles(roles)
+                        .phoneNumbers(phoneNumbers)
+                    .build();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void save(User user) {
+        File usersDir = new File("users");
+
+        if (usersDir.exists()) {
+            FileFilter fileFilter = file -> file.getName().endsWith(".user");
+            File[] userFiles = usersDir.listFiles(fileFilter);
+
+            if (userFiles != null) {
+                long lastUserId = getLastUserId(userFiles);
+
+                if (lastUserId != -1) {
+                    String newUserFileName = (lastUserId + 1) + ".user";
+                    File userFile = new File(usersDir, newUserFileName);
+                    saveUserToFile(user, userFile);
+                }
+            }
+        } else if (usersDir.mkdir()) {
+            File userFile = new File(usersDir, "1.user");
+            saveUserToFile(user, userFile);
+        }
+    }
+
     @Override
     public void update(User user) {
+        File usersDir = new File("users");
 
+        if (usersDir.exists()) {
+            String fileName = user.getId() + ".user";
+            File userFile = new File(usersDir, fileName);
+            saveUserToFile(user, userFile);
+        }
     }
 
     @Override
@@ -92,6 +126,24 @@ public class UserFileRepository implements UserRepository {
 
     @Override
     public User getById(long id) {
+        File usersDir = new File("users");
+
+        if (usersDir.exists()) {
+            String fileName = id + ".user";
+            File userFile = new File(usersDir, fileName);
+            try {
+                String userFileText = new String(Files.readAllBytes(Paths.get(userFile.getCanonicalPath())));
+                User user = parseUserFileText(userFileText);
+
+                if (user != null) {
+                    user.setId(id);
+                }
+
+                return user;
+            } catch (IOException ignored) {
+            }
+        }
+
         return null;
     }
 }
